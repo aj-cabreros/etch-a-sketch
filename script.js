ba1062429bfd93a2e20d982da71681e416a5b829
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-08-16 16:09:08 -1000

    [+] Add eventListeners to each div tile to be colored Blue
    Added inline mouseover event attriubute to each tile (div) creacted via DOM manipulation. Used the mouseover attribute will call function "colorTile" to color the tile that was mouseovered.
    [Ticket: X]

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
 
 /* N and n is number of rows & columns */
 const DEFAULT_N = 16;
+const DEFAULT_COLOR = "blue";
+
 let n = DEFAULT_N;
 console.log(typeof(DEFAULT_N));
 
 
 let grid = document.querySelector(".container");
-
-
 let columns;
 let rows;
 let tileNumber = 1;
 
-for( let i = 1 ; i <= n ; i++){
-    columns = document.createElement("div");
-    columns.classList.add("column");
-    grid.appendChild(columns);
 
-    for (let j = 1 ; j <= n ; j++){
-        rows = document.createElement("div");
-        rows.classList.add("tile");
-        columns.appendChild(rows);
-        
+createGrid();
+
+
+
+function createGrid(){
+    
+    for( let i = 1 ; i <= n ; i++){
+        columns = document.createElement("div");
+        columns.classList.add("column");
+        grid.appendChild(columns);
+    
+        for (let j = 1 ; j <= n ; j++){
+            rows = document.createElement("div");
+            rows.classList.add("tile");
+            rows.setAttribute("id", "tile" + tileNumber);
+            rows.setAttribute("onmouseover", "colorTile(this.id)");
+            tileNumber++;
+            columns.appendChild(rows);
+            
+        }
     }
+}
+
+function colorTile(ID){
+    console.log(ID);
+    let currentTile = document.querySelector("#" + ID);
+    currentTile.style.backgroundColor = DEFAULT_COLOR;
+
 
 }
+
+
+
+
+
+
+
 
